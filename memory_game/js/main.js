8867d8c468712a6924862aa49bad79e544c52e8b
luthandoroxo <roxoluthando@gmail.com>
2017-07-19 14:21:56 +0200

    create arrays on main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,28 @@
- console.log("Up and running!");
+ // console.log("Up and running!");
 
- var cardOne = "king";
- var cardTwo = "king";
- var cardThree = "queen";
- var cardFour = "queen";
+ // var cardOne = "king";
+ // var cardTwo = "king";
+ // var cardThree = "queen";
+ // var cardFour = "queen";
 
- console.log("User flipped " + cardOne);
+ // console.log("User flipped " + cardOne);
+
+ var cards = ["queen", "queen", "king", "king"];
+
+ var cardsInPlay = [];
+ cardsInPlay.push("queen");
+ console.log("User flipped queen");
+
+ var cardOne = ["queen"];
+ // console.log("User flipped queen");
+
+ var cardTwo = [].push("queen");
+ console.log("User flipped king");
+
+ if (cardsInPlay.length === 2) {
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+ 		alert("You found a match");
+ 	} else {
+ 		alert("Sorry, try again.");
+ 	}
+ }
